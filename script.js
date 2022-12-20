5089bd1ddeff315b0033465fa6c3358e7ed0aa9d
nafisreza <bigganinafis@gmail.com>
2022-12-20 21:35:57 +0600

    Section Reveal Animation Added

--- script.js
+++ script.js
@@ -49,3 +49,23 @@ const headerObserver = new IntersectionObserver(sticky, {
 })
 
 headerObserver.observe(header);
+
+/////////////////////////////////////////////////////////////
+// Section Reveal Animation
+/////////////////////////////////////////////////////////////
+
+function revealSection(entries){
+    const entry = entries[0]
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove('section--hidden');
+}
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.2
+})
+
+allSections.forEach((section) => {
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+} )
